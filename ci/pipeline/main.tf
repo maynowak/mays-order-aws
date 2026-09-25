1a8ca3b5754b0f1ba97bbfb2f5c61822c6d7c39c
maynowak <nowakbewerbung@gmail.com>
2026-09-25 14:45:32 +0200

    ci: add source to Verify stage and copy deployment_context from deploy_output

--- ci/pipeline/main.tf
+++ ci/pipeline/main.tf
@@ -518,10 +518,11 @@ resource "aws_codepipeline" "main" {
       owner            = "AWS"
       provider         = "CodeBuild"
       version          = "1"
-      input_artifacts  = ["deploy_output"]
+      input_artifacts  = ["source_output", "deploy_output"]
       output_artifacts = ["verify_output"]
       configuration = {
-        ProjectName = aws_codebuild_project.verify.name
+        ProjectName   = aws_codebuild_project.verify.name
+        PrimarySource = "source_output"
       }
       run_order = 1
     }
